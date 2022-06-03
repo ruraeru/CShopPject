783770d0e3dad4d361b2592be9a609c395b80d3a
ruraeru <f93335383@gmail.com>
2022-06-03 15:06:27 +0900

    초기 커밋.

---
+++ CShopPject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CShopPject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
