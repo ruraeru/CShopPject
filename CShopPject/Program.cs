e4d67333e2be779f609f623eee2481252d074611
ruraeru <f93335383@gmail.com>
2022-06-03 15:25:23 +0900

    test

--- CShopPject/Program.cs
+++ CShopPject/Program.cs
@@ -6,7 +6,7 @@ namespace CShopPject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Woasdsadsarld!");
         }
     }
 }
